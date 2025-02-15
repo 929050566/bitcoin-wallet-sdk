2004fe19438332cf1af6caa2c38943cfb31a518a
Draven <929050566@qq.com>
2025-02-15 11:40:41 +0800

    edit README.md

--- src/bitcoinv2/sign.ts
+++ src/bitcoinv2/sign.ts
@@ -1,5 +1,13 @@
 const bitcore = require('bitcore-lib');
 // @ts-ignore
+import * as bitcoin from 'bitcoinjs-lib';
+import BIP32Factory from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+
+import { toXOnly, tapTreeToList, tapTreeFromList } from "bitcoinjs-lib/src/psbt/bip371"
+
+bitcoin.initEccLib(ecc);
+const bip32 = BIP32Factory(ecc);
 
 interface TransactionInput {
     address: string;
@@ -47,4 +55,41 @@ export function signUtxoTransaction (params: { privateKey: string; signObj: Sign
     transaction.version = 2;
     transaction.sign(privateKey);
     return transaction.toString();
+}
+
+
+export async function signBtcTaprootTransaction(params: any) {
+    const { signObj, privateKey } = params
+    const psbt = new bitcoin.Psbt({network: bitcoin.networks.bitcoin});
+
+    const inputs = signObj.inputs.map((input: any) => {
+        return {
+            hash: input.txid,
+            index: input.index,
+            witnessUtxo: {value: input.amount, script: input.output!},
+            tapInternalKey: toXOnly(input.publicKey),
+        }
+    });
+    psbt.addInputs(inputs);
+
+    const sendInternalKey = bip32.fromPrivateKey(privateKey, Buffer.from("0"));
+
+    const output = signObj.output.map((output : any) => {
+        return {
+            value: output.value,
+            address: output.sendAddress!,
+            tapInternalKey: output.sendPubKey,
+        }
+    });
+
+    psbt.addOutputs(output);
+
+    const tweakedSigner = sendInternalKey.tweak(
+        bitcoin.crypto.taggedHash('TapTweak', toXOnly(sendInternalKey.publicKey)),
+    );
+
+    await psbt.signInputAsync(0, tweakedSigner);
+    psbt.finalizeAllInputs();
+    const tx = psbt.extractTransaction();
+    return tx.toBuffer().toString('hex');
 }
