de922909c14cb03f71779cd9dde0fec2119595ab
Draven <929050566@qq.com>
2025-02-20 21:58:14 +0800

    format code

--- test/sui/address.test.ts
+++ test/sui/address.test.ts
@@ -1,7 +1,5 @@
 import { createKeypair, createKeypairFromSeed, createKeypairFromPrivateKey, createKeypairFromSeed2 } from '../../src/sui/address';
 import * as bip39 from 'bip39';
-import * as bip32 from 'bip32';
-import { derivePath } from 'ed25519-hd-key';
 
 describe('Sui Address Module', () => {
   test('createKeypair should generate a valid keypair', () => {
@@ -21,9 +19,7 @@ describe('Sui Address Module', () => {
     const seed = bip39.mnemonicToSeedSync(mnemonic);
     // 使用 bip44 派生路径 m/44'/784'/0'/0'/0' 生成 seedHex
     const path = "m/74'/784'/0'/0'/0'"; // 默认路径
-    const kp0 = createKeypairFromSeed(seed.toString('hex'));
-    console.log('kp0===:', kp0);
-    const kp = createKeypairFromSeed2(mnemonic, path);
+    const kp = createKeypairFromSeed(seed.toString('hex'));
     console.log('kp===:', kp);
     expect(kp.address).toBeDefined();
     expect(typeof kp.address).toBe('string');
@@ -36,4 +32,3 @@ describe('Sui Address Module', () => {
     expect(recovered.address).toEqual(original.address);
   });
 });
-0xe5b22f50be1b40ca00a8a452ed9ae68c47ab80c90c70d6cd651b06f3e9eaaed6
